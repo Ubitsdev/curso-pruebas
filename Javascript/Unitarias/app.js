e672837189a3d1d014d4a9da70fe3fa705ce74cd
aminespinoza10 <amin.espinoza@outlook.com>
2022-11-17 17:11:57 -0600

    branch updated

---
+++ Javascript/Unitarias/app.js
@@ -0,0 +1,70 @@
+const { checkAndGenerateAge, checkAndGeneratePhone, checkAndGenerateMail, createElement  } = require('./util');
+
+const initApp = () => {
+  // Inicia la app y registra los eventos a los botones
+  const newAgeButton = document.querySelector('#btnAddAge');
+  newAgeButton.addEventListener('click', addAge);
+
+  const newPhoneButton = document.querySelector('#btnAddPhone');
+  newPhoneButton.addEventListener('click', addPhone);
+
+  const newMailButton = document.querySelector('#btnAddMail');
+  newMailButton.addEventListener('click', addMail);
+};
+
+const addAge = () => {
+  const newUserNameInput = document.querySelector('input#name1');
+  const newUserAgeInput = document.querySelector('input#age');
+
+  const outputText = checkAndGenerateAge(
+    newUserNameInput.value,
+    newUserAgeInput.value
+  );
+
+  if (!outputText) {
+    return;
+  }
+
+  const userList = document.querySelector('.user-list');
+  const element = createElement('li', outputText, 'user-item');
+  userList.appendChild(element);
+};
+
+const addPhone = () => {
+  const newUserNameInput = document.querySelector('input#name2');
+  const newUserPhoneInput = document.querySelector('input#phone');
+
+  const outputText = checkAndGeneratePhone(
+    newUserNameInput.value,
+    newUserPhoneInput.value
+  );
+
+  if (!outputText) {
+    return;
+  }
+
+  const userList = document.querySelector('.user-list');
+  const element = createElement('li', outputText, 'user-item');
+  userList.appendChild(element);
+};
+
+const addMail = () => {
+  const newUserNameInput = document.querySelector('input#name3');
+  const newUserMailInput = document.querySelector('input#mail');
+
+  const outputText = checkAndGenerateMail(
+    newUserNameInput.value,
+    newUserMailInput.value
+  );
+
+  if (!outputText) {
+    return;
+  }
+
+  const userList = document.querySelector('.user-list');
+  const element = createElement('li', outputText, 'user-item');
+  userList.appendChild(element);
+};
+
+// Start the app!
+initApp();
